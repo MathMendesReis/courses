698ba3baf5bf0ff4b5423b86c82a22860636f0cc
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-13 06:52:57 -0300

    Adicionar DTO UpdateActiveRequestDTO para atualização do status ativo de cursos

---
+++ src/main/java/com/rocketseat/courses/modules/courses/dto/UpdateActiveRequestDTO.java
@@ -0,0 +1,13 @@
+package com.rocketseat.courses.modules.courses.dto;
+
+public class UpdateActiveRequestDTO {
+  private boolean active;
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+}
