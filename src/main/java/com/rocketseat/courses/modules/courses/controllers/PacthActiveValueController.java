0048c39da9afeb8df4db59a171debad337a918a7
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-13 14:07:51 -0300

    Adicionar documentação Swagger ao PacthActiveValueController para detalhar a resposta da atualização de valor ativo

--- src/main/java/com/rocketseat/courses/modules/courses/controllers/PacthActiveValueController.java
+++ src/main/java/com/rocketseat/courses/modules/courses/controllers/PacthActiveValueController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rocketseat.courses.modules.courses.dto.UpdateActiveRequestDTO;
 import com.rocketseat.courses.modules.courses.useCases.PacthActiveValueUseCase;
 import com.rocketseat.courses.modules.courses.utils.ApiRouters;
-
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -23,10 +25,13 @@ public class PacthActiveValueController {
   private PacthActiveValueUseCase pacthActiveValueUseCase;
   @PatchMapping("/{id}")
   @Tag(name = "Cursos",description = "Atualiza o valor de ativo de um curso pelo seu id")
+    @ApiResponses({
+        @ApiResponse(responseCode = "204")
+    })
   public ResponseEntity<?> handle(@RequestBody UpdateActiveRequestDTO request,@PathVariable("id") UUID id) {
     try {
       this.pacthActiveValueUseCase.execute(request,id);
-      return new ResponseEntity<>(null, HttpStatus.CREATED);
+      return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
